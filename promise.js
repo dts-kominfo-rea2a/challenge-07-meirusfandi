7ed410e946100ddb5448ba3e52a34d89764e7c76
Mei Rusfandi <mei.rusfandi@bankmandiri.co.id>
2022-06-12 09:54:14 +0700

    update promise js

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+  let result = 0;
+  let combinesIXXandVGC = ixx.concat(vgc);
+  combinesIXXandVGC.map(combinesIXXandVGC => {
+      if (combinesIXXandVGC.hasil === emotion) 
+        result++;
+    }
+  )
+  return result;
+};
 
 module.exports = {
   promiseOutput,
